5dd3ca2efaafb9b64f2a7f1088a22e9523277df0
gael <cgvalenzuelac@concredito.com.mx>
2021-01-30 00:18:33 -0700

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
 import './App.css';
+import socketIOClient from "socket.io-client";
+import { useEffect, useState } from 'react';
+
+const ENDPOINT = "ws://localhost:88/";
 
 function App() {
+  const [ response,setResponse]= useState<String[]>([]);
+  useEffect(() => {     
+    const socket =socketIOClient.io(ENDPOINT);
+    socket.on("events", (data: Array<String>) => {
+      console.log(data);      
+      setResponse(data);
+    });
+    
+  },[]);
   return (
     <div className="App">
-    <span>listo...</span>
+      <span>listo!</span>
+      {response.map(d => <b>{d}</b>)}
     </div>
   );
 }
